3affb22f2d1537d4c2df7b6fe2bcb6d68a098acf
MasterPabu <venkat@localhost.localdomain>
2016-05-23 21:06:00 +0000

    SysAd Task 1

---
+++ delta1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir sysad
+touch sysad/file{1..100}
+truncate -s 10240 sysad/file*
+for file in sysad/file*
+do
+	head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16 > $file
+	sudo chmod 444 $file
+	touch -d "2 days ago" $file
+done
